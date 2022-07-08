1ae9907ef6d893f5af42e6565941ff6f2e1ad92b
Ryan Fickenscher <rsficken@gmail.com>
2022-07-07 22:12:58 -0700

    Section 2, Reading User Input

---
+++ README.md
@@ -0,0 +1,3 @@
+# Hecto
+A tutorial by Philipp Flenker demonstrating Rust by writing a small text editor based on the C language, *kilo* tutorials.
+[Tutorial Source](https://www.philippflenker.com/hecto/)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::io::{ self, stdout, Read };
+use termion::raw::IntoRawMode;
+
+fn to_ctrl_byte(c: char) -> u8 {
+    let byte = c as u8;
+    byte & 0b0001_1111
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Set terminal into raw mode
+    let _stdout = stdout().into_raw_mode().unwrap();
+    
+    for b in io::stdin().bytes() {
+        let b = b.unwrap();
+        let c = b as char;
+
+        if c.is_control() {
+            println!("{:?} \r", b);
+        } else {
+            println!("{:?} ({})\r", b, c);
+        }
+        if b == to_ctrl_byte('q') {
+            break;
+        }
+    }
 }
