232af1168a5c41cb2b090d8bd13ea23a4b852b65
Ryan Fickenscher <rsficken@gmail.com>
2022-07-11 22:06:04 -0700

    chapter 2, editor and terminal

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,11 @@
-use std::io::{ self, stdout, Read };
-use termion::raw::IntoRawMode;
+#![warn(clippy::all, clippy::pedantic)]
+mod editor;
+mod terminal;
 
-fn to_ctrl_byte(c: char) -> u8 {
-    let byte = c as u8;
-    byte & 0b0001_1111
-}
+use editor::Editor;
+pub use terminal::Terminal;
+pub use editor::Position;
 
 fn main() {
-    // Set terminal into raw mode
-    let _stdout = stdout().into_raw_mode().unwrap();
-    
-    for b in io::stdin().bytes() {
-        let b = b.unwrap();
-        let c = b as char;
-
-        if c.is_control() {
-            println!("{:?} \r", b);
-        } else {
-            println!("{:?} ({})\r", b, c);
-        }
-        if b == to_ctrl_byte('q') {
-            break;
-        }
-    }
+   Editor::default().run();
 }
